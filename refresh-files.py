2f3df88f8b4010bbbe677d449b3a7358d97a30ec
wtucker <wtbtucker@gmail.com>
2022-08-31 12:41:36 -0400

    Move refresh files into function

--- refresh-files.py
+++ refresh-files.py
@@ -4,30 +4,40 @@ import csv
 import glob
 
 # Set path to current file location
-path = os.path.dirname(os.path.realpath(__file__))
-Inpath = path + '\\FBA Templates'
-all_files = glob.glob(os.path.join(Inpath, '*.tsv'))
+def main():
+    df = import_templates()
 
-df = pd.DataFrame()
-for file in all_files:
-    with open(file, "r") as f:
-        reader = csv.reader(f, delimiter='\t')
-        for row in reader:
-            if len(row) and row[0] == 'Shipment ID':
-                ShippingId = row[1]
-            elif len(row) and row[0] =='Name':
-                sep = '-'
-                FBA_name = row[1].split(sep, 1)[0]
-                break
-        template = pd.read_table(f, delimiter='\t', header=4)
-        template.insert(10,'ShippingID', ShippingId)
-        df = pd.concat([df,template])
-df = df.rename(columns={'Shipped':'OnOrder'})
+    sorting_df = df.copy()
+    sorting_df['Difference'], sorting_df['Sorted'] = [sorting_df['OnOrder'], 0]
 
-sorting_df = df.copy()
-sorting_df['Difference'], sorting_df['Sorted'] = [sorting_df['OnOrder'], 0]
+    boxing_df = df.copy()
+    boxing_df['Difference'], boxing_df['Boxed'] = [boxing_df['OnOrder'], 0]
 
-boxing_df = df.copy()
-boxing_df['Difference'], boxing_df['Boxed'] = [boxing_df['OnOrder'], 0]
+# TODO: Add functionality for when the sorting app is opened
+# Current FBA orders created by refresh files
+# format of OpenFile and OpenBox file and when  they are created?
+# format of temp files
 
-print(boxing_df)
+def import_templates():
+    path = os.path.dirname(os.path.realpath(__file__))
+    Inpath = path + '\\FBA Templates'
+    all_files = glob.glob(os.path.join(Inpath, '*.tsv'))
+
+    df = pd.DataFrame()
+    for file in all_files:
+        with open(file, "r") as f:
+            reader = csv.reader(f, delimiter='\t')
+            for row in reader:
+                if len(row) and row[0] == 'Shipment ID':
+                    ShippingId = row[1]
+                elif len(row) and row[0] =='Name':
+                    sep = '-'
+                    FBA_name = row[1].split(sep, 1)[0]
+                    break
+            template = pd.read_table(f, delimiter='\t', header=4)
+            template.insert(10,'ShippingID', ShippingId)
+            df = pd.concat([df,template])
+    df = df.rename(columns={'Shipped':'OnOrder'})
+    return df
+
+main()
