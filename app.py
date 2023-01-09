0d8ac72944c2323ee24f2d65786763b02fe998be
wtbtucker <wtbtucker@gmail.com>
2023-01-09 18:34:33 -0500

    Move sql cursor commands into the main function. This change separates lines related to SQL (creating table and inserting data into table) from lines related to reading the csv file. By separating these functions it will be possible to iterate over the csv files in the templates folder without necessarily executing the SQL commands as well.

--- app.py
+++ app.py
@@ -10,55 +10,52 @@ def main():
 
     # Open a SQLite connection and create cursor
     with sqlite3.connect(dbfile) as conn:
-        prepare_data(conn, csv_file)
+        
+        # Create cursor for sqlite queries
+        cursor = conn.cursor()
 
-def prepare_data(conn, csv_file):
-    # Create cursor for sqlite queries
-    db = conn.cursor()
-
-    # Create sqlite table
-    create_table = '''CREATE TABLE template(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        merchantsku TEXT NOT NULL,
-        title TEXT NOT NULL,
-        asin TEXT NOT NULL,
-        upc TEXT NOT NULL,
-        whoprep TEXT NOT NULL,
-        preptype TEXT NOT NULL,
-        label TEXT NOT NULL,
-        onorder TEXT NOT NULL);'''
-    db.execute(create_table)
-
-    # Open CSV file and append data to the sqlite table
-    open_template(csv_file, conn)
+        # Open template file from csv into pandas dataframe
+        data_df = open_template(csv_file)
 
 
-    
+        # Create SQLite table 'template'
+        create_table = '''CREATE TABLE IF NOT EXISTS template(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            merchantsku TEXT NOT NULL,
+            title TEXT NOT NULL,
+            asin TEXT NOT NULL,
+            ean TEXT NOT NULL,
+            whoprep TEXT NOT NULL,
+            preptype TEXT NOT NULL,
+            label TEXT NOT NULL,
+            onorder TEXT NOT NULL);'''
+        cursor.execute(create_table)
 
-def open_template(csv_file, connection):  
+        # Insert the dataframe into the template SQL table
+        data_df.to_sql('template', conn, if_exists='append')
+
+        # Select row from table
+        selection = cursor.execute('SELECT * FROM template LIMIT 2;')
+        print(selection)
+
+# Open csv file and return dataframe
+def open_template(csv_file):
+
+    # Use specified columns and rename to fit SQL schema
     data = pd.read_csv(csv_file, index_col=0, header=7, usecols=[0,1,2,4,6,7,8,9], dtype={'external-id': str}, sep='\t')
-    
     data = data.rename(columns={
         'Title': 'title',
     	'ASIN': 'asin',
-        'external-id': 'upc',
+        'external-id': 'ean',
         'Who will prep?': 'whoprep',
         'Prep Type': 'preptype',
         'Who will label?': 'label',
         'Shipped': 'onorder'
     })
+
+    # Rename column 0 to fit SQL schema
     data.index.names = ['merchantsku']
-
-    data.to_sql('template', connection, if_exists='append')
-
-
-# SQL query to insert list of strings into SQL table
-def insert_records(content):
-    global db
-    print(content)
-    sql_args = '?, ?, ?, ?, ?, ?, ?, ?'
-    records = f'INSERT INTO person VALUES ({sql_args})'
-    db.execute(records, content)
+    return data
 
 
 main()
